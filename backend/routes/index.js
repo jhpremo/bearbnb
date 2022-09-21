348b6b5acd1eaef1d290f817d36b0fd115c2555e
jason <jh.premo@gmail.com>
2022-09-21 15:27:20 -0400

    added API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
@@ -9,5 +10,9 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
+
 
 module.exports = router;
